7b2906897ca13ec6a2b5321e3a42740de20158c7
Erik Bergsten <erikbergsten94@gmail.com>
2022-03-27 11:20:08 +0200

    add poll interval to webpack watch options

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src')+'/index.ts',
   watch: true,
+  watchOptions: {
+    poll: 1000
+  },
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
